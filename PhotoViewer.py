21bf26c3f3883c1e43ce5061839238f2a326b048
bhand <sushbhand123@gmail.com>
2020-09-01 20:16:19 +0200

    updated status to view the image no. in the GUI

--- PhotoViewer.py
+++ PhotoViewer.py
@@ -15,6 +15,8 @@ image_list = [myImg1, myImg2, myImg3]
 my_label = Label(image=myImg1)
 my_label.grid(row=0, column=0, columnspan=3)
 
+status = Label(win, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+
 
 def forward(image_num):
     global my_label
@@ -33,6 +35,9 @@ def forward(image_num):
     button_back.grid(row=4, column=0)
     button_forward.grid(row=4, column=1)
 
+    status = Label(win, text="Image " + str(image_num) +" of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=6, column=0, columnspan=3, sticky=W + E)
+
 
 def backward(image_num):
     global my_label
@@ -45,11 +50,14 @@ def backward(image_num):
     button_back = Button(win, text="<<", command=lambda: backward(image_num - 1))
 
     if image_num == 1:
-        button_back = Button(win, text="<<", state = DISABLED)
+        button_back = Button(win, text="<<", state=DISABLED)
 
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=4, column=0)
     button_forward.grid(row=4, column=1)
+
+    status = Label(win, text="Image " + str(image_num) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=6, column=0, columnspan=3, sticky=W + E)
 
 
 button_back = Button(win, text="<<", command=backward, state=DISABLED)
@@ -58,6 +66,7 @@ button_forward = Button(win, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=4, column=0)
 button_forward.grid(row=4, column=1)
-button_exit.grid(row=4, column=2)
+button_exit.grid(row=4, column=2, pady=10)
+status.grid(row=6, column=0, columnspan=3, sticky=W+E)
 
 win.mainloop()
